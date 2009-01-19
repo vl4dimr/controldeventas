aaee7f746747205f01a77a4d1904c394379c4cd4
novalys <novalys@aabc5062-cd1b-11dd-8829-37334faa441c>
2009-01-19 14:57:27 +0000

    Revision

---
+++ Control Inventario/factura.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Control_Inventario
+{
+    class factura
+    {
+        public string id { get; set; }
+        public string cliente { get; set; }
+        public string importe { get; set; }
+        public string fecha { get; set; }
+    }
+}
